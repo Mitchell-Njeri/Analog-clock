5878b789a9f80a26a24d57465a2f77e7379e7f38
Mitchell-Njeri <mitchellnyam@gmail.com>
2023-04-11 09:39:30 +0300

    clock

--- clock.js
+++ clock.js
@@ -1,21 +1,33 @@
-let secondHand=document.querySelector("#seconds");
-let minHand=document.querySelector("#minutes");
-let hourHand=document.querySelector("#hours");
+let secondHand=document.querySelector("#sec")
+let minHand=document.querySelector("#min")
+let hourHand=document.querySelector("#hr")
 
-setInterval(clockRotating, 1000);
+setInterval(clockRotating, 1000)
 
 function clockRotating(){
-   var date=new Date();
-   var getSeconds=date.getSeconds()*6;
-   var getMinutes=date.getMinutes()*6;
-   var getHours=date.getHours()*30;
+    var date=new Date();
+    var getSeconds=date.getSeconds()/60;
+    var getMinutes=date.getMinutes()/60;
+    var getHours=date.getHours()/12;
 
-   secondHand.style.transform="rotate("+getSeconds*360 +"deg)";
-   hourHand.style.transform="rotate("+getHours*360 +"deg)";
-   minHand.style.transform="rotate("+getMinutes*360 +"deg)";
+    secondHand.style.transform="rotate("+getSeconds*360 +"deg)"
+    hourHand.style.transform="rotate("+getHours*360 +"deg)"
+    minHand.style.transform="rotate("+getMinutes*360 +"deg)"
 
 
-   console.log(getSeconds);
-   console.log(getMinutes);
-   console.log(getHours);
+
+    
+   
+
+    // console.log(getSeconds)
+    // console.log(getMinutes)
+    // console.log(getHours)
+
+
+
+
+
+
+
+
 }
